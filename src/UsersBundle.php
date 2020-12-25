4ba9a183082897c0022b258c142f0185f58c1f3c
StenFredd <dvbakaev@gmail.com>
2020-12-25 15:56:51 +0300

    Initial commit.

---
+++ src/UsersBundle.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace lavarich\UsersBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class UsersBundle extends Bundle
+{
+    public function __construct()
+    {
+
+    }
+}
